c6031ef98fdfd2249931b48819ca003a7d6b99f7
Mohammad-Asraful <asrafmail01@gmail.com>
2023-03-03 15:55:07 +0600

    add responsive design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { ImWindows } from "react-icons/im";
-import { IoLogoFigma } from "react-icons/io5";
 import { FcGoogle } from "react-icons/fc";
+import { FiFigma } from "react-icons/fi";
 import Profile from '../src/Image/profile.jpg'
 
 function App() {
@@ -22,7 +22,10 @@ function App() {
             <small>Start your 30 day free trial</small>
           </div>
 
-          <button className='px-2 py-1 mb-3 flex items-center gap-2 rounded-full border w-full justify-center font-bold sm:text-sm'>
+          <button className='px-2 py-1 mb-3 flex items-center gap-2 rounded-full border w-full justify-center font-bold text-xs 
+          lg:text-lg
+          md:text-base
+          sm:text-sm'>
             <FcGoogle /> Sign up with Google
           </button>
 
@@ -49,14 +52,24 @@ function App() {
 
             <button className='rounded-full border px-2 py-1 w-full bg-blue-700 text-white'>Create account</button>
 
-            <p className='text-center mt-2 sm:text-sm'>Been here before? <a className='font-bold' href="@">Log in</a></p>
+            <p className='text-center mt-2 text-xs 
+            lg:text-lg
+            md:text-base
+            sm:text-sm
+            '>Been here before? <a className='font-bold' href="@">Log in</a></p>
 
           </form>
 
-          <div className='flex lg:flex-row md:flex-row sm:flex-col sm:items-center justify-between mt-5'>
+          <div className='mt-5 flex flex-col items-center justify-between text-xs
+          lg:flex-row lg:text-lg 
+          md:flex-row md:text-base
+          sm:flex-col sm:items-center'>
             <p>&copy; 2022 Rejiggle</p>
 
-            <div className='flex lg:flex-row md:flex-row sm:flex-col lg:gap-3 md:gap-3 sm:gap-0'>
+            <div className='flex flex-col 
+            lg:flex-row  lg:gap-3 
+            md:flex-row md:gap-3  
+            sm:flex-col sm:gap-0'>
               <a href='@'>Terms</a>
               <a href='@'>Privacy</a>
             </div>
@@ -72,17 +85,22 @@ function App() {
           <div>
 
             <div className='text-emerald-500 w-8 h-8 bg-white grid justify-center items-center rounded-full mb-2 text-xl'>
-              <IoLogoFigma />
+              <FiFigma />
             </div>
 
             <h1 className='font-bold lg:text-5xl lg:w-96 md:w-48 sm:w-48 sm:text-2xl'>Save thousand of design hours</h1>
 
-            <p className='opacity-70 mt-5
-              lg:w-96 lg:text-base md:w-52 md:text-base 
+            <p className='opacity-70 mt-5 text-xs
+              lg:w-96 lg:text-base 
+              md:w-52 md:text-base 
               sm:w-48 sm:text-sm'>Join thousands of designers and mockup high-quality landing pages in minutes</p>
           </div>
 
-          <div className='lg:flex lg:flex-row md:flex md:flex-row md:items-center sm:flex sm:flex-col sm:items-center gap-3 mt-4'>
+          <div className='mt-4 flex flex-col items-center
+          lg:flex lg:flex-row 
+          md:flex md:flex-row md:items-center 
+          sm:flex sm:flex-row sm:items-center 
+          gap-3'>
             <div className='flex'>
               <img className='w-[20px] h-[20px] rounded-full' src={Profile} alt="This is a profile" />
               <img className='w-[20px] h-[20px] rounded-full -ml-2' src={Profile} alt="This is a profile" />
